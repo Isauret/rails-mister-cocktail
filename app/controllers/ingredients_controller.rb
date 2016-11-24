3e49999f9d14e0cdc58f0d26a6e07824659d6e37
Isaure Troesch <isauretroesch@gmail.com>
2016-11-24 17:19:34 +0100

    Delete controllers fuck

--- app/controllers/ingredients_controller.rb
+++
@@ -1,9 +0,0 @@
-class IngredientsController < ApplicationController
-
-  def create
-    @ingredient = Ingredient.new(ingredient_params)
-    @ingredient.save
-
-    # redirect_to cocktail_path(@cocktail)
-  end
-end
